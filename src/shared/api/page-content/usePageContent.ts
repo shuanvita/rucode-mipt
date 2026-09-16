789421f39c6b5678c846ef40555ba1685a420068
Andrey Shurygin <shuanvita@gmail.com>
2026-09-16 10:00:30 +0300

    upd

--- src/shared/api/page-content/usePageContent.ts
+++ src/shared/api/page-content/usePageContent.ts
@@ -6,7 +6,10 @@ export function usePageContent(slug: string, fallback: CmsPage) {
       return { page: await $fetch<CmsPage>(`/api/cms${slug}`), isFallback: false }
     } catch (e) {
       if (import.meta.dev)
-        console.error(`[api] ошибка получения контента ${slug}, используются данные из проекта`, e)
+        console.error(
+          `[api] ошибка получения контента ${slug}, используются локальные данные из проекта`,
+          e,
+        )
       return { page: fallback, isFallback: true }
     }
   })
